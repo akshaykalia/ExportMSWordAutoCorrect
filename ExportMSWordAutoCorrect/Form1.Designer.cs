7a0c49e8ef86f4992f2501eef4a9c3c188297ec1
Akshay Kalia <akshay.kalia@nyu.edu>
2015-11-20 00:40:31 -0500

    Export MS Word AutoCorrects to CSV
    Export MS Word AutoCorrects to CSV

---
+++ ExportMSWordAutoCorrect/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ExportMSWordAutoCorrect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblACCount = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.BackColor = System.Drawing.Color.LightBlue;
+            this.btnFetch.Location = new System.Drawing.Point(54, 31);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(230, 23);
+            this.btnFetch.TabIndex = 0;
+            this.btnFetch.Text = "[1] Fetch AutoCorrects from MS Word";
+            this.btnFetch.UseVisualStyleBackColor = false;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(290, 31);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(13, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 250);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(54, 63);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.ReadOnly = true;
+            this.txtLocation.Size = new System.Drawing.Size(311, 20);
+            this.txtLocation.TabIndex = 3;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(371, 61);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(100, 23);
+            this.btnSelectFolder.TabIndex = 4;
+            this.btnSelectFolder.Text = "Save Location";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnSave.Location = new System.Drawing.Point(477, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "[2] Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 250);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(371, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // lblACCount
+            // 
+            this.lblACCount.AutoSize = true;
+            this.lblACCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblACCount.Location = new System.Drawing.Point(51, 97);
+            this.lblACCount.Name = "lblACCount";
+            this.lblACCount.Size = new System.Drawing.Size(0, 13);
+            this.lblACCount.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 403);
+            this.Controls.Add(this.lblACCount);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFetch);
+            this.Name = "Form1";
+            this.Text = "Save MS Word AutoCorrects to CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblACCount;
+    }
+}
+
---
+++ ExportMSWordAutoCorrect/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Word = Microsoft.Office.Interop.Word;
+using System.ComponentModel;
+using System.Drawing;
+using CsvHelper;
+
+namespace ExportMSWordAutoCorrect
+{
+    public partial class Form1 : Form
+    {
+        Word.Application application;
+        static List<AutoCorrects> items;
+        CheckBox checkboxHeader;
+        static int TotalClicked = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtLocation.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\MSWord_Autocorrects.csv";
+        }
+
+        #region DataGridView Events
+        void checkboxHeader_Click(object sender, EventArgs e)
+        {
+            bool value = ((CheckBox)dataGridView1.Controls.Find("checkboxHeader", true)[0]).Checked;
+           
+            foreach (DataGridViewRow Row in dataGridView1.Rows)
+            {
+                Row.Cells[0].Value = value;
+            }
+            TotalClicked = value ? items.Count : 0;
+            dataGridView1.RefreshEdit();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0 && e.RowIndex > -1)
+            {
+                
+                    //Escalate Editmode
+                    this.dataGridView1.EndEdit();
+                    string re_value = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].FormattedValue.ToString();
+                    if (re_value.ToLower() == "true")
+                    {
+                        dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "false";
+                        checkboxHeader.Checked = false;
+                        TotalClicked--;
+                        
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "true";
+                        TotalClicked++;
+                        if (TotalClicked == items.Count)
+                        {
+                            checkboxHeader.Checked = true;
+                        }
+                    }
+
+                
+            }
+        }
+        #endregion
+
+        #region Button Events
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            btnFetch.Enabled = false;
+            btnClear.Enabled = false;
+            btnSave.Enabled = false;
+            btnSelectFolder.Enabled = false;
+            try
+            {
+                progressBar1.Maximum = 100;
+                progressBar1.Step = 1;
+                progressBar1.Value = 0;
+                // Clean old DataGridView 
+                dataGridView1.DataSource = null;
+                dataGridView1.Columns.Clear();
+                dataGridView1.Refresh();
+                lblACCount.Text = string.Empty;
+                backgroundWorker.RunWorkerAsync();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtLocation.Text = folderBrowserDialog1.SelectedPath + "\\MSWord_Autocorrects.csv";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            List<AutoCorrects> selected = new List<AutoCorrects>();
+            foreach (DataGridViewRow Row in dataGridView1.Rows)
+            {
+                if (Row.Cells[0].FormattedValue.ToString().ToLower() == "true")
+                {
+                    selected.Add(new AutoCorrects
+                    {
+                        Key = Row.Cells[1].FormattedValue.ToString(),
+                        Value = Row.Cells[2].FormattedValue.ToString()
+                    });
+                }
+            }
+            if (selected.Count > 0)
+            {
+                if(File.Exists(txtLocation.Text))
+                    File.Delete(txtLocation.Text);
+                using (TextWriter writer = File.CreateText(txtLocation.Text))
+                {
+                    var csv = new CsvWriter(writer);
+                    csv.Configuration.HasHeaderRecord = false;
+                    csv.Configuration.QuoteAllFields = true;
+                    csv.WriteRecords(selected);
+                }
+                MessageBox.Show("File saved successfully !");
+            }
+            else
+            {
+                MessageBox.Show("No AutoCorrects Selected");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            lblACCount.Text = string.Empty;
+        }
+        #endregion
+
+        #region Get MS Word AutoCorrects
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            application = new Word.Application();
+            var backgroundWorker = sender as BackgroundWorker;
+            GetAutoCorrectEntries(application);
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            object missing = Type.Missing;
+            // prevents Word from trying to save the Normal.dotm template
+            application.NormalTemplate.Saved = true;
+            ((Microsoft.Office.Interop.Word._Application)application).Quit(missing, missing, missing);
+
+            btnFetch.Enabled = true;
+            btnClear.Enabled = true;
+            btnSave.Enabled = true;
+            btnSelectFolder.Enabled = true;
+            progressBar1.Value = 0;
+            if (items.Count > 0)
+            {
+                var bindingList = new BindingList<AutoCorrects>(items);
+                var source = new BindingSource(bindingList, null);
+                dataGridView1.DataSource = source;
+                GenerateDGV();
+                lblACCount.Text = this.dataGridView1.Rows.Count + " AutoCorrects found";
+            }
+        }
+
+        private void GenerateDGV()
+        {
+            dataGridView1.EditMode = DataGridViewEditMode.EditOnKeystrokeOrF2;
+
+            // Adding column to select/deselect all auto-corrects
+            DataGridViewCheckBoxColumn checkboxColumn = new DataGridViewCheckBoxColumn();
+            checkboxColumn.Width = 30;
+            checkboxColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns.Insert(0, checkboxColumn);
+
+            // add checkbox header
+            Rectangle rect = dataGridView1.GetCellDisplayRectangle(0, -1, true);
+            // set checkbox header to center of header cell. +1 pixel to position correctly.
+            rect.X = rect.Location.X + (rect.Width / 4);
+
+            checkboxHeader = new CheckBox();
+            checkboxHeader.Name = "checkboxHeader";
+            checkboxHeader.Size = new Size(18, 18);
+            checkboxHeader.Location = rect.Location;
+            checkboxHeader.Checked = true;
+            checkboxHeader.Click += checkboxHeader_Click;
+
+            dataGridView1.Controls.Add(checkboxHeader);
+
+
+            this.dataGridView1.Columns[0].Width = 30;
+            this.dataGridView1.Columns[1].Width = 250;
+            this.dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells[0].Value = true;
+            }
+            TotalClicked = dataGridView1.Rows.Count;
+        }
+
+        private int GetAutoCorrectEntries(Word.Application application)
+        {
+            int TotalACEntries = -1;
+            try
+            {
+                // If MS Word is not installed properly or some registry issue
+                // http://answers.microsoft.com/en-us/office/forum/office_2010-word/unable-to-cast-com-object-of-type/34efcd57-e819-4a83-9cdd-501ab15b0971
+                TotalACEntries = application.AutoCorrect.Entries.Count;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("In the Windows Control Panel, go to Programs & Features. Select the Office 2010 icon and click Change. On the first page of the wizard, click Repair and then Continue. Let it finish and run app again.");
+                return -1;
+            }
+            items = new List<AutoCorrects>();
+            for (int x = 1; x <= TotalACEntries; x++)
+            {
+                object index = (object)x;
+                items.Add(new AutoCorrects
+                {
+                    Key = application.AutoCorrect.Entries.get_Item(ref index).Name,
+                    Value = application.AutoCorrect.Entries.get_Item(ref index).Value
+                });
+                backgroundWorker.ReportProgress((x * 100) / TotalACEntries);
+            }
+            return TotalACEntries;
+        }
+        #endregion
+
+    }
+
+    public class AutoCorrects
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+}
